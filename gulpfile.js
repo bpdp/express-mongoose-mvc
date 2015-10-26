791d51f4eaa47a0c4f0056ec6f22bb737e66bcd1
Bambang Purnomosidi D. P <bambangpdp@yahoo.com>
2015-10-27 05:17:12 +0700

    Adjust to the newest version as of current push date

--- gulpfile.js
+++ gulpfile.js
@@ -26,14 +26,14 @@ if ('development' == env) {
 
 // JS hint task
 gulp.task('jshint', function() {
-  gulp.src('*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+    gulp.src('*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
 
 gulp.task('default', function() {
 
-	http.createServer(app).listen(app.get('port'))
-  console.log("Express server listening on port " + app.get('port'));
+    http.createServer(app).listen(app.get('port'))
+    console.log("Express server listening on port " + app.get('port'));
 
 });
